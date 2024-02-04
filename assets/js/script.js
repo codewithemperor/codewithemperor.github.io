819d6d3227fde827d3d07f16880f3a7b61c24407
Sulaimon Yusuf <codewithemperor@gmail.com>
2024-02-04 14:20:44 +0100

    modified:   assets/js/script.js 	new file:   resources/js/three.min.js 	new file:   resources/js/vanta.cells.min.js 	new file:   resources/js/vanta.dots.min.js 	new file:   resources/js/vanta.globe.min.js 	new file:   resources/js/vanta.rings.min.js 	new file:   resources/js/vanta.topology.min.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,67 @@
-$(document).ready(function() {
-  // Function to toggle the 'on' class on the sidebar
-  function toggleSidebar() {
-      $('.sidebar').toggleClass('on');
-  }
+// VANTA GLOBE
 
-  // Toggle the sidebar when the toggler button is clicked
-  $('.toggler').on('click', function(event) {
-      event.stopPropagation(); // Prevent the click event from propagating to document
-      toggleSidebar();
-  });
+VANTA.GLOBE({
+  el: "#your-element-selector",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  size: 1.50
+})
 
-  // Hide the sidebar when clicking outside of it
-  $(document).on('click', function(event) {
-      if (!$(event.target).closest('.sidebar').length && !$(event.target).hasClass('toggler')) {
-          $('.sidebar').removeClass('on');
-      }
-  });
-  
-  // Prevent the click event on the sidebar from closing it
-  $('.sidebar').on('click', function(event) {
-      event.stopPropagation();
-  });
-});
+// VANTA CELL
+VANTA.CELLS({
+  el: "#your-element-selector",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  size: 1.30,
+  speed: 1.80
+})
 
 
+// VANTA DOTS
+VANTA.DOTS({
+  el: "#your-element-selector",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  size: 4.40,
+  spacing: 24.00,
+  showLines: false
+})
 
-let toggle = document.querySelector(".toggle");
-let navigation = document.querySelector(".sidebar");
-let main = document.querySelector(".main-body");
+// VANTA TOPOLOGY
+VANTA.TOPOLOGY({
+  el: "#your-element-selector",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00
+})
 
-if (toggle) {
-  // If 'toggle' is not null, assign the click event handler
-  toggle.onclick =  () => {
-      navigation.classList.toggle("active");
-      main.classList.toggle("active");
-  };
-} 
 
-// navigation.onclick = function(){
-//   navigation.style.width = "30px";
-// } 
-
-// ***************************** //
-// Select and input script
-// ***************************** //
-
-function toggleInput(selectElement) {
-  var inputElement = selectElement.parentElement.nextElementSibling.querySelector('input');
-
-  if (selectElement.value === 'Yes') {
-    inputElement.removeAttribute('disabled');
-    inputElement.style.display = 'block'; // or 'inline-block', 'inline', depending on your layout
-  } else {
-    inputElement.setAttribute('disabled', 'disabled');
-    inputElement.value = ''; // Reset the input value
-    inputElement.style.display = 'none';
-  }
-}
-
+// VANTA RINGS
+VANTA.RINGS({
+  el: "#your-element-selector",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00
+})
